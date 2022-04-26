7509216f6216e818bff55ea6e467219e335cfe2f
J-mung <xorb2298@gmail.com>
2022-04-26 22:18:27 +0900

    Add Broadcast Receiver Example

---
+++ app/src/main/java/com/example/androidstudy/BroadCastReceiverExam.java
@@ -0,0 +1,38 @@
+package com.example.androidstudy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.IntentFilter;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class BroadCastReceiverExam extends AppCompatActivity {
+
+    public static TextView tv_state;
+    private NetworkReceiver receiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_broad_cast_receiver_exam);
+
+        tv_state = (TextView) findViewById(R.id.tv_state);
+
+        // 브로드 캐스트 리시버 객체 생성
+        IntentFilter filter = new IntentFilter();
+        receiver = new NetworkReceiver();
+        filter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION);
+        // 브로드 캐스트 리시버 등록
+        registerReceiver(receiver, filter);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // 브로드 캐스트 리시버 해제
+        unregisterReceiver(receiver);
+    }
+}
